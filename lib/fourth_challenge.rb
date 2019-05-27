8af866a1e10456f926444b600cc45ebc8ae5d47a
Tom White <eugene.white@gmail.com>
2019-05-27 01:07:45 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -8,7 +8,7 @@ def fourth_challenge
         {name: "Benvolio",
         age: "17",
         attitude: "worried"
-        }
+        },
         {name: "Mercutio",
         age: "18",
         attitude: "hot-headed"}
@@ -22,7 +22,7 @@ def fourth_challenge
         {name: "Steven",
           age: "30",
           attitude: "confused"
-        }
+        },
         {name: "Nurse",
         age: "44",
         attitude: "worried"}
